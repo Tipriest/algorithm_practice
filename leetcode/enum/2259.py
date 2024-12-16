9f86c600e96dd579f423209b70c2186d17be59b8
Tipriest <a1503741059@163.com>
2024-12-16 21:20:24 +0800

    solve a simple leetcode

---
+++ leetcode/enum/2259.py
@@ -0,0 +1,20 @@
+from typing import List
+from queue import Queue
+class Solution:
+    def removeDigit(self, number: str, digit: str) -> str:
+        n = len(number)
+        res = ""
+        for i in range(n):
+            if number[i] == digit:
+                temp:str = number[:i]+number[i+1:]
+                res = max(res, temp)
+        return res
+                
+                    
+if __name__ == "__main__":
+    solution = Solution()
+    number:str = "123"
+    digit:str = '3'
+    result = solution.removeDigit(number, digit)
+    print(f"result = {result}")
+ 
