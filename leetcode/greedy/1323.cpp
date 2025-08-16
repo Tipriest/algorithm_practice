eba5f6d0bfe23edd5ced61136a861f957fe4de6d
Tipriest <a1503741059@163.com>
2025-08-16 19:01:50 +0800

    add daily leetcode

---
+++ leetcode/greedy/1323.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <functional> //包含std::greater 和 std::less
+#include <iostream>
+#include <limits.h>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+  int maximum69Number(int num) {
+    string s = to_string(num);
+    int i = s.find('6');
+    if(i == string::npos){
+        return num;
+    }
+    s[i] = '9';
+    return stoi(s);
+  }
+};
+
+int main() {
+  // 示例二叉树
+  Solution solution;
+  vector<int> nums = {7, 1, 5, 3, 6, 4};
+  int result = solution.maximum69Number(9669);
+  std::cout << "result: " << result << std::endl;
+}
