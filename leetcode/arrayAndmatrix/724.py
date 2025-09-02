20fbc5572435c92e07104c8feabce4b1973cd15c
Tipriest <a1503741059@163.com>
2025-09-02 21:06:05 +0800

    solve a simple leetcode

---
+++ leetcode/arrayAndmatrix/724.py
@@ -0,0 +1,20 @@
+from typing import List
+from queue import Queue
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total:int = sum(nums)
+        temp:int = 0
+        n:int = len(nums)
+        for i in range(n):
+            if temp*2 == total-nums[i]:
+                return i
+            else:
+                temp += nums[i]
+        return -1
+            
+if __name__ == "__main__":
+    solution = Solution()
+    nums = [1,7,3,6,5,6]
+    result = solution.pivotIndex(nums)
+    print(f"result = {result}")
+ 
