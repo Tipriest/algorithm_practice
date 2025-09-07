b662154bc85f56ffee5a11c9c44602a798317017
Tipriest <a1503741059@163.com>
2025-09-07 17:36:47 +0800

    solve a simple leetcode

---
+++ leetcode/binaryTree/lcp44.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <functional> //包含std::greater 和 std::less
+#include <iostream>
+#include <limits.h>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+  unordered_set<int> uset;
+  void calC(TreeNode *cur) {
+    if(nullptr == cur){
+      return;
+    }
+    uset.insert(cur->val);
+    calC(cur->left);
+    calC(cur->right);
+  }
+  int numColor(TreeNode *root) {
+    calC(root);
+    return uset.size();
+  }
+};
+int main() {
+  // 示例二叉树
+  Solution solution;
+  TreeNode *root = new TreeNode(1);
+  root->left = new TreeNode(1);
+  root->left->left = new TreeNode(1);
+  root->left->right = new TreeNode(1);
+  root->right = new TreeNode(1);
+  root->right->right = new TreeNode(1);
+  bool result = solution.numColor(root);
+  std::cout << "result: " << result << std::endl;
+  return 0;
+}
