f72aa0a0203cfab7df4a93c7f68f240f49cfc9df
Tipriest <a1503741059@163.com>
2025-09-15 14:39:40 +0800

    solve a simple leetcode

---
+++ leetcode/string/1844.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <functional> //包含std::greater 和 std::less
+#include <iostream>
+#include <limits.h>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+  string replaceDigits(string s) {
+    int n = s.size();
+    for (int i = 1; i < n; i += 2) {
+      s[i] = s[i - 1] + (s[i] - '0');
+    }
+    return s;
+  }
+};
+int main() {
+  // 示例二叉树
+  Solution solution;
+  vector<int> nums = {7, 1, 5, 3, 6, 4};
+  string result = solution.replaceDigits("a1b2c3d4e");
+  std::cout << "result: " << result << std::endl;
+  return 0;
+}
