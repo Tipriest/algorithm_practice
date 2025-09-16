43ac4fe1e6e094648aac72aeb3c655cd1d6fff73
Tipriest <a1503741059@163.com>
2025-09-16 15:02:33 +0800

    solve a hard leetcode

---
+++ leetcode/math/2197.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <functional> //包含std::greater 和 std::less
+#include <iostream>
+#include <limits.h>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+  vector<int> replaceNonCoprimes(vector<int> &nums) {
+    vector<int> st;
+    for (int num : nums) {
+      while(!st.empty() && 1 != gcd(num, st.back())){
+         num = lcm(num, st.back());
+         st.pop_back();
+      }
+      st.push_back(num);
+    }
+    return st;
+  }
+};
+int main() {
+  // 示例二叉树
+  Solution solution;
+  vector<int> nums = {7, 1, 5, 3, 6, 4};
+  vector<int> result = solution.replaceNonCoprimes(nums);
+  return 0;
+}
