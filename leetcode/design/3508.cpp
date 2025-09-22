803e9aa95ad977289e7d7af23d10f39eac760268
Tipriest <a1503741059@163.com>
2025-09-22 16:49:31 +0800

    solve a middle leetcode

--- leetcode/design/3508.cpp
+++ leetcode/design/3508.cpp
@@ -10,15 +10,6 @@
 
 using namespace std;
 
-void printPackQueue(queue<Pack> pque) {
-  while (!pque.empty()) {
-    cout << "timestamp: " << pque.front().timestamp
-         << "source: " << pque.front().source
-         << "destination: " << pque.front().destination << endl;
-    pque.pop();
-  }
-  cout << endl;
-}
 struct Pack {
   Pack(int a, int b, int c) { source = a, destination = b, timestamp = c; }
   int timestamp;
@@ -30,7 +21,7 @@ struct Pack {
   }
 };
 
-struct Packhash {
+struct PackHash {
   std::size_t operator()(const Pack &pack) const {
     std::size_t h1 = std::hash<int>{}(pack.source);
     std::size_t h2 = std::hash<int>{}(pack.destination);
@@ -40,7 +31,29 @@ struct Packhash {
     return h1 ^ (h2 << 1) ^ (h3 << 2);
   }
 };
-
+struct PackHash2 {
+  template <typename T> static void hash_combine(size_t &seed, const T &v) {
+    // 参考boost::hash_combine
+    seed ^= hash<T>{}(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+  }
+  std::size_t operator()(const Pack &pack) const { 
+    size_t seed = 0;
+    hash_combine(seed, pack.timestamp);
+    hash_combine(seed, pack.source);
+    hash_combine(seed, pack.destination);
+    // 组合hash values
+    return seed;
+  }
+};
+void printPackQueue(queue<Pack> pque) {
+  while (!pque.empty()) {
+    cout << "timestamp: " << pque.front().timestamp
+         << "source: " << pque.front().source
+         << "destination: " << pque.front().destination << endl;
+    pque.pop();
+  }
+  cout << endl;
+}
 class Router {
 public:
   Router(int memoryLimit) { this->memory_limit = memoryLimit; }
@@ -53,19 +66,11 @@ public:
     }
 
     // 拿一个队列向里面填东西
-    // 队列里现在不够memory_limit，可以直接填
-    if (uset.size() < memory_limit) {
-      que.push(p);
-      uset.insert(p);
-      umap[p.destination].push_back(p);
-      return true;
+    // 队列的东西太多了
+    // 吐一个出来
+    if (memory_limit == que.size()) {
+      forwardPacket();
     }
-    // 够memory_limit，需要先推一个出来
-    while (uset.find(que.front()) == uset.end()) {
-      que.pop();
-    }
-    uset.erase(que.front());
-    que.pop();
     que.push(p);
     uset.insert(p);
     umap[p.destination].push_back(p);
@@ -76,53 +81,79 @@ public:
     if (0 == que.size()) {
       return {};
     }
-    // 够memory_limit，需要先推一个出来
-    while (uset.find(que.front()) == uset.end()) {
-      que.pop();
-    }
     Pack p = que.front();
     que.pop();
     uset.erase(p);
-    umap[p.destination].push_back(p);
+    umap[p.destination].pop_front();
     return {p.source, p.destination, p.timestamp};
   }
-
-  int getCount(int destination, int startTime, int endTime) {
-    int result = 0;
-    for (const Pack &p : umap[destination]) {
-      if (uset.find(p) == uset.end()) {
-        continue;
-      }
-      if (p.timestamp >= startTime && p.timestamp <= endTime) {
-        result++;
+  int dichotomyL(const deque<Pack> &packs, int startTime, int endTime) {
+    int l = 0;
+    int r = packs.size() - 1;
+    while (l <= r) {
+      int mid = (l + r) / 2;
+      if (packs[mid].timestamp < startTime) {
+        l = mid + 1;
+      }else if (packs[mid].timestamp >= startTime) {
+        if (0 == mid) {
+          return 0;
+        } else {
+          if (packs[mid - 1].timestamp < startTime) {
+            return mid;
+          } else {
+            r = mid - 1;
+          }
+        }
       }
     }
-    return result;
+    // didn't find, return -1
+    return -1;
+  }
+  int dichotomyR(const deque<Pack> &packs, int startTime, int endTime) {
+    int l = 0;
+    int r = packs.size() - 1;
+    while (l <= r) {
+      int mid = (l + r) / 2;
+      if (packs[mid].timestamp > endTime) {
+        r = mid - 1;
+      }else if (packs[mid].timestamp <= endTime) {
+        if (packs.size() - 1 == mid) {
+          return mid;
+        } else {
+          if (packs[mid + 1].timestamp > endTime) {
+            return mid;
+          } else {
+            l = mid + 1;
+          }
+        }
+      }
+    }
+    // didn't find, return -1
+    return -1;
+  }
+  int getCount(int destination, int startTime, int endTime) {
+    int result = 0;
+    int l = dichotomyL(umap[destination], startTime, endTime);
+    int r = dichotomyR(umap[destination], startTime, endTime);
+    if (-1 == l || -1 == r) {
+      return 0;
+    }
+    return r - l + 1;
   }
 
   int memory_limit = 0;
   queue<Pack> que;
-  unordered_set<Pack, Packhash> uset;
-  unordered_map<int, vector<Pack>> umap;
+  unordered_set<Pack, PackHash2> uset;
+  unordered_map<int, deque<Pack>> umap;
 };
 int main() {
   // 示例二叉树
-  Router *obj = new Router(2);
-  obj->addPacket(7, 4, 90);
+  Router *obj = new Router(4);
+  obj->addPacket(4, 2, 1);
   printPackQueue(obj->que);
-  obj->addPacket(2, 5, 90);
+  obj->addPacket(3, 2, 1);
   printPackQueue(obj->que);
-  obj->addPacket(1, 4, 90);
-  printPackQueue(obj->que);
-  obj->addPacket(3, 5, 95);
-  printPackQueue(obj->que);
-  obj->addPacket(4, 5, 105);
-  printPackQueue(obj->que);
-  vector<int> res = obj->forwardPacket();
-  printPackQueue(obj->que);
-  obj->addPacket(5, 2, 110);
-  printPackQueue(obj->que);
-  obj->getCount(5, 100, 110);
+  obj->getCount(2, 1, 1);
   printPackQueue(obj->que);
   return 0;
 }
