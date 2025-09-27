96b91f94c81484212e24aee20aab69c53ce15f75
Tipriest <a1503741059@163.com>
2025-09-27 22:13:14 +0800

    solve two simple leetcode

---
+++ leetcode/weekComp/250927/q2.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <functional> //包含std::greater 和 std::less
+#include <iostream>
+#include <limits.h>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+  long long maxTotalValue(vector<int> &nums, int k) {
+    long long result = 0;
+    return ((long long)*max_element(nums.begin(), nums.end()) -
+            (long long)*min_element(nums.begin(), nums.end()))*(long long)k;
+  }
+};
+int main() {
+  // 示例二叉树
+  Solution solution;
+  vector<int> nums = {7, 3, 9, 9, 7, 3, 5, 9, 7, 2, 6, 10, 9, 7, 9,
+                      1, 3, 6, 2, 4, 6, 2, 6, 8, 4, 8, 2,  7, 5, 6};
+  int result = solution.maxTotalValue(nums, 2);
+  std::cout << "result: " << result << std::endl;
+  return 0;
+}
