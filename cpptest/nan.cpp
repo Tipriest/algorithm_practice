2e9a91fbd5c3b826cdad79c5eba49a5f8c77c03b
Tipriest <a1503741059@163.com>
2025-10-05 22:33:43 +0800

    test nan in cpp

---
+++ cpptest/nan.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iostream>
+#include <unordered_set>
+#include <set>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <functional> //包含std::greater 和 std::less
+#include <math.h>
+#include <limits.h>
+using namespace std;
+struct TreeNode {
+   int val;
+   TreeNode *left;
+   TreeNode *right;
+   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+   TreeNode(int x, TreeNode *left, TreeNode *right)
+       : val(x), left(left), right(right) {}
+};
+struct ListNode {
+   int val;
+   ListNode *next;
+   ListNode() : val(0), next(nullptr) {}
+   ListNode(int x) : val(x), next(nullptr) {}
+   ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+int main() {
+   // 示例二叉树
+   double x = std::sqrt(-1.0); // 这将产生 NaN
+   std::cout << "x: " << x << std::endl;
+}
