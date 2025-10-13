8aa41edcf35fb9037d440670c38da5761a3442ed
Tipriest <a1503741059@163.com>
2025-10-13 19:41:22 +0800

    add timeget file as test

---
+++ cpplibs/time/main.cpp
@@ -0,0 +1,41 @@
+#include "timeMarker.h"
+#include <algorithm>
+#include <functional> //包含std::greater 和 std::less
+#include <iostream>
+#include <limits.h>
+#include <math.h>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+int main() {
+  long long ut = getSystemTime();
+  double t = getTimeSecond();
+
+  while (true) {
+    ut = getSystemTime();
+    t = getTimeSecond();
+    usleep(1e6);
+    cout << "ut = " << ut << endl;
+    cout << "t = " << (long long)t << endl;
+  }
+}
