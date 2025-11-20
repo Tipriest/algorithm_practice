dfaf8ac0de9162b0a343ff1af5178012f0141063
Tipriest <a1503741059@163.com>
2025-11-20 20:40:09 +0800

    update viser 00_getting_started ch

---
+++ pylibs/00_getting_started/00_helloworld.py
@@ -0,0 +1,24 @@
+import time
+
+import viser
+
+
+def main():
+    server = viser.ViserServer()
+    server.scene.add_icosphere(
+        name="/hello_sphere",
+        radius=0.5,
+        color=(255, 0, 0),  # Red
+        position=(0.0, 0.0, 0.0),
+    )
+
+    print("Open your browser to http://localhost:8080")
+    print("Press Ctrl+C to exit")
+
+    while True:
+        time.sleep(10.0)
+
+
+if __name__ == "__main__":
+    main()
+    
